25e3207fc42646a8f75f6417ae5966a6a1c784a0
Aspin Chakma <aspinchakma2019@gmail.com>
2025-10-23 10:38:32 +0600

    get only 3 companies data

--- src/Components/TrustedBy.jsx
+++ src/Components/TrustedBy.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
+
 const TrustedBy = () => {
+  const [companies, setCompanies] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/companies/3")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
   return (
-    <div>
-      <h3>hello createBrowserRouter</h3>
+    <div className="w-[90%] lg:w-[77%] mx-auto">
+      <h3 className="text-center text-[28px] font-medium">
+        Featured Companies
+      </h3>
+      <p className="text-center text-[20px] text-[#888] mt-1">
+        Always interested in meeting new talent
+      </p>
     </div>
   );
 };
