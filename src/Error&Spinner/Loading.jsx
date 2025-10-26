16f212588535b1c79e1b6ef16f30ee44fb264b54
Aspin Chakma <aspinchakma2019@gmail.com>
2025-10-26 15:59:16 +0600

    finished footer

--- src/Error&Spinner/Loading.jsx
+++ src/Error&Spinner/Loading.jsx
@@ -1,7 +1,10 @@
 const Loading = () => {
   return (
-    <div>
-      <h3 className="text-4xl font-bold">Data Loading....</h3>
+    <div className="lg:w-[77%] w-[90%] mx-auto flex items-center justify-center my-10">
+      <h3 className="text-4xl font-bold">
+        Data Loading
+        <span className="loading loading-dots loading-xl"></span>
+      </h3>
     </div>
   );
 };
